1edd2417b79f4664a23194076a5228f7aa26c3a8
Matt Kelly <matt.r.kelly27@gmail.com>
2019-02-14 01:57:33 +0000

    add chatty_server files

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,48 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
-
   constructor(props) {
-
     super(props);
-   
     this.state = {
       currentUser: {name: "Bob"}, 
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     };
-  };
+    this.addNewMessage = this.addNewMessage.bind(this);
+  }
+    
+  
+  addNewMessage(content) {
+    console.log("we are in the app.jsx") 
+    const newMessage = {id: Date.now(), username: content.username, content: content.content};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages })
+    // this.socket.send(`User ${newMessage.username} said ${newMessage.content}!`);
+    let msg = JSON.stringify(content);
+    this.socket.send(msg);
 
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket.onmessage = (event) => {
+      console.log(event);
+      // code to handle incoming message
+    }
+  }
+
+  componentDidMount(content) {
+    this.socket = new WebSocket(`ws://${window.location.hostname}:3001`);
+    this.socket.onopen = (content) => {
+      console.log("hello!");
+    }
+    
+
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
   }
 
   render() {
@@ -45,7 +54,7 @@ class App extends Component {
       <a href="/" className="navbar-brand">Chatty</a>
     </nav>
       <MessageList messages={this.state.messages}/>
-      <ChatBar user={this.state.currentUser.name}/> 
+      <ChatBar user={this.state.currentUser.name} addNewMessage={this.addNewMessage}/> 
   </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,18 +1,33 @@
-{/* <footer className="chatbar">
-<input className="chatbar-username" placeholder="Your Name (Optional)" />
-<input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-</footer> */}
-
-
 import React, { Component } from "react";
+
 class ChatBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: ""
+        }
+    }
+
+    onSubmit = evt => {
+        evt.preventDefault();
+        const content = {
+          username: evt.target.elements.username.value,
+          content: evt.target.elements.newMessage.value,
+          id: Date.now()
+        };
+        this.props.addNewMessage(content);
+        evt.target.elements.newMessage.value = "";
+    };
+
     render() {
         return(
-
-    <footer className="chatbar">
-    <input className="chatbar-username" placeholder={this.props.user} />
-    <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    </footer>
+            <form onSubmit={this.onSubmit}>
+                <footer className="chatbar">
+                    <input className="chatbar-username" placeholder={this.props.user} name="username" />
+                    <input className="chatbar-message" placeholder="Type a message and hit ENTER" name="newMessage" />
+                    <input type="submit" className="chatbar-button" />
+                </footer>
+            </form>
         );
     }
 }
