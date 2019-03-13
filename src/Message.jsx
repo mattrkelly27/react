48ffbe53c17e6fa8104abb956e1a30f320f34d89
Matt Kelly <matt.r.kelly27@gmail.com>
2019-03-13 17:18:22 +0000

    add function -- if username changes

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 class Message extends Component {
+
+    ifNewUser() {
+        if(this.props.user !== this.props.message.username)
+        return(
+            <div className="notification">
+            <span className="notification-content">TO BE SURE: {this.props.user} changed their name to {this.props.message.username}</span>
+            </div>
+        )
+    }
+
     render() {  
         return(
             <div className="message">
                 <span className="message-username">{this.props.message.username}</span>
                 <span className="message-content">{this.props.message.content}</span>
-                <div className="notification">
-                    <span className="notification-content">TO BE SURE: {this.props.user} changed their name to {this.props.message.username}</span>
-                </div>
+               
             </div>
         );
     }
